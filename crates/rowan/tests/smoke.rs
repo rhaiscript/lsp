c8b0d8fad0f9480eb71e22a47b1504976ace0e5c
tamasfe <me@tamasfe.dev>
2022-06-29 23:06:54 +0200

    chore(tests): example scripts and snapshot tests

--- crates/rowan/tests/smoke.rs
+++ crates/rowan/tests/smoke.rs
@@ -1,11 +1,16 @@
 use rhai_rowan::parser::Parser;
 use test_case::test_case;
 
-#[test_case(include_str!("../../../testdata/valid/simple.rhai"))]
-#[test_case(include_str!("../../../testdata/valid/oop.rhai"))]
-fn parse_valid(src: &str) {
+#[test_case("simple", include_str!("../../../testdata/valid/simple.rhai"))]
+#[test_case("oop", include_str!("../../../testdata/valid/oop.rhai"))]
+fn parse_valid(name: &str, src: &str) {
     let parse = Parser::new(src).parse_script();
     assert!(parse.errors.is_empty());
-    // TODO(tests)
-    // insta::assert_snapshot!(format!("{:#?}", parse.into_syntax()));
+
+    insta::with_settings!(
+        { snapshot_suffix => name },
+        {
+            insta::assert_snapshot!(format!("{:#?}", parse.into_syntax()));
+        }
+    );
 }
